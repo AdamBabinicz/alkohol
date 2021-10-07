733e6fd748b126408cb72fab745a5e2625ba5183
babinicz <babinicz@onet.pl>
2021-10-07 17:39:01 +0200

    zm

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Footer from "./components/Footer";
 import Privacy from "./components/Privacy";
 import CookieConsent from "react-cookie-consent";
 import Thank from "./components/Thank";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
@@ -19,49 +20,50 @@ function App() {
           <Route path="/privacy" component={Privacy} />
           <Route path="/dziekujemy-za-wiadomosc" component={Thank} />
         </Switch>
-      </Router>
-      <Home />
-      <About />
-      <Service />
-      <Project />
-      <Blog />
-      <Contact />
-      <Footer />
-      <CookieConsent
-        debug={true}
-        location="bottom"
-        style={{
-          background: "#333",
-          textAlign: "left",
-          paddingBottom: "1rem",
-        }}
-        buttonStyle={{
-          color: "#333",
-          background: "#fff",
-          fontSize: "14px",
-          marginRight: "1rem",
-        }}
-        buttonText="OK, rozumiem"
-        expires={365}
-      >
-        “W ramach naszej witryny stosujemy pliki cookies w celu świadczenia
-        Państwu usług na najwyższym poziomie, w tym w sposób dostosowany do
-        indywidualnych potrzeb. Korzystanie z witryny bez zmiany ustawień
-        dotyczących cookies oznacza, że będą one zamieszczane w Państwa
-        urządzeniu końcowym. Możecie Państwo dokonać w każdym czasie zmiany
-        ustawień dotyczących cookies. Więcej  szczegółów w naszej
-        <a
-          href="/privacy"
-          // target="_blank"
-          rel="noopener noreferrer"
+
+        <Home />
+        <About />
+        <Service />
+        <Project />
+        <Blog />
+        <Contact />
+        <Footer />
+        <CookieConsent
+          debug={true}
+          location="bottom"
           style={{
-            color: "rgb(204, 204, 204)",
-            textDecoration: "none",
+            background: "#333",
+            textAlign: "left",
+            paddingBottom: "1rem",
           }}
+          buttonStyle={{
+            color: "#333",
+            background: "#fff",
+            fontSize: "14px",
+            marginRight: "1rem",
+          }}
+          buttonText="OK, rozumiem"
+          expires={365}
         >
-          &nbsp;„Polityce Cookies”/”Polityce Prywatności”.
-        </a>
-      </CookieConsent>
+          “W ramach naszej witryny stosujemy pliki cookies w celu świadczenia
+          Państwu usług na najwyższym poziomie, w tym w sposób dostosowany do
+          indywidualnych potrzeb. Korzystanie z witryny bez zmiany ustawień
+          dotyczących cookies oznacza, że będą one zamieszczane w Państwa
+          urządzeniu końcowym. Możecie Państwo dokonać w każdym czasie zmiany
+          ustawień dotyczących cookies. Więcej  szczegółów w naszej
+          <Link
+            to="/privacy"
+            // target="_blank"
+            rel="noopener noreferrer"
+            style={{
+              color: "rgb(204, 204, 204)",
+              textDecoration: "none",
+            }}
+          >
+            &nbsp;„Polityce Cookies”/”Polityce Prywatności”.
+          </Link>
+        </CookieConsent>
+      </Router>
     </>
   );
 }
